9fdbf35e0e73209477b09198b201c673d78b9f52
Vansh1067 <50477887+Vansh1067@users.noreply.github.com>
2020-02-16 17:27:39 +0530

    Add More Member button is added

--- index.js
+++ index.js
@@ -13,21 +13,31 @@ const groupNamesBackButton=document.getElementById('groupNamesBackButton');
 const addMemberName=document.getElementById('addMemberName');
 const addMemberNameAddButton=document.getElementById('addMemberNameAddButton');
 const addMemberNameResetButton=document.getElementById('addMemberNameResetButton');
-function addExpenses(){
+const addMemberNameInput=document.querySelectorAll('.input-content input');
+const addgroupNameInput=document.querySelectorAll('.Registration input');
+const addExpensesInput=document.querySelectorAll('.Expenses input');
+const addButtonImg=document.getElementById('addButtonImg');
+const inputDiv=document.querySelector('.input-content');
+function addExpensesCancel(){
     buttonUI.classList.toggle('hide');
     expensesUI.classList.toggle('hide'); 
+    reset(addExpensesInput);
 }
 function addGroup(){
 buttonUI.classList.toggle('hide');
 RegistrationUI.classList.toggle('hide');
+reset(addgroupNameInput);
 }
 function addReport(){
     buttonUI.classList.toggle('hide');
     reportUI.classList.toggle('hide'); 
+    
+    
 }
 function addMembersName(){
     RegistrationUI.classList.toggle('hide');
     addMemberName.classList.toggle('hide');
+    reset(addMemberNameInput);
 
 }
 function addMemberNameButton(){
@@ -35,11 +45,29 @@ function addMemberNameButton(){
     buttonUI.classList.toggle('hide');
 
 }
+function reset(arrays){
+    arrays.forEach(event=>{
+            event.value="";
+    });
+}
+function addExpensesDetails(){
+    addExpensesCancel();
+
+}
+function addMoreMember(){
+     inputMember=document.createElement("input");
+     inputMember.setAttribute('type','text');
+     inputMember.setAttribute('placeholder','Enter Member Name.....');
+     inputDiv.appendChild(inputMember);
+}
 newRegistrationButton.addEventListener('click',addGroup);
-expensesButton.addEventListener('click',addExpenses);
+expensesButton.addEventListener('click',addExpensesCancel);
 reportButton.addEventListener('click',addReport);
 addGroupCancelButton.addEventListener('click',addGroup);
-addExpensesCancelButton.addEventListener('click',addExpenses);
+addExpensesCancelButton.addEventListener('click',addExpensesCancel);
+addExpensesAddButton.addEventListener('click',addExpensesDetails);
 groupNamesBackButton.addEventListener('click',addReport);
 addGroupAddButton.addEventListener('click',addMembersName);
 addMemberNameAddButton.addEventListener('click',addMemberNameButton);
+addMemberNameResetButton.addEventListener('click',reset.bind(this,addMemberNameInput));
+addButtonImg.addEventListener('click',addMoreMember);
